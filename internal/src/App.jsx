582341474e0bdf6b35d13802596ac2894aedcd19
Yulian-Zhu-yu <117557803+Yulian-Zhu-yu@users.noreply.github.com>
2025-02-02 20:14:33 -0700

    Updated for 2025 Mecha Mayhem

--- internal/src/App.jsx
+++ internal/src/App.jsx
@@ -10,19 +10,18 @@ const App = () => {
   const [statusType, setStatusType] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const [cropCount, setCropCount] = useState(0);
-  const [baleCount, setBaleCount] = useState(0);
-  const [parkCount, setParkCount] = useState(0);
-  const [bonus, setBonus] = useState(0);
-  const [bonusToggled, setBonusToggled] = useState(false);
+  const [chickenCount, setChickenCount] = useState(0);
+  const [cowCount, setCowCount] = useState(0);
+  const [clearPlotBonus, setClearPlot] = useState(false);
+  const [doorClosedBonus, setDoor] = useState(false);
+  const [parkBonus, setPark] = useState(false);
 
   const [menuVisible, setMenuVisible] = useState(false);
-  const [toggledButton, setToggledButton] = useState('');
 
   useEffect(() => {
-    const totalScore = cropCount + (baleCount * 4) + parkCount + bonus;
+    const totalScore = 10 * (chickenCount + cowCount + clearPlotBonus + doorClosedBonus + parkBonus);
     setScore(totalScore);
-  }, [cropCount, baleCount, parkCount, bonus]);
+  }, [chickenCount, cowCount, clearPlotBonus, doorClosedBonus, parkBonus]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -51,52 +50,51 @@ const App = () => {
       setAge('');
       setEmail('');
       setPhone('');
-      setCropCount(0);
-      setBaleCount(0);
-      setParkCount(0);
-      setBonus(0);
-      setBonusToggled(false);
+      setChickenCount(0);
+      setCowCount(0);
+      setClearPlot(false);
+      setDoor(false);
+      setPark(false);
       setToggledButton('');
     }
     setIsSubmitting(false);
     setTimeout(() => setStatus(''), 8000);
   };
 
-  const incrementCropCount = () => {
-    if (cropCount < 18) {
-      setCropCount(cropCount + 1);
+  const incrementChickenCount = () => {
+    if (chickenCount < 4) {
+      setChickenCount(chickenCount + 1);
     }
   };
 
-  const decrementCropCount = () => {
-    if (cropCount > 0) {
-      setCropCount(cropCount - 1);
+  const decrementChickenCount = () => {
+    if (chickenCount > 0) {
+      setChickenCount(chickenCount - 1);
     }
   };
 
-  const toggleBonus = () => {
-    if (bonusToggled) {
-      setBonus(0);
-    } else {
-      setBonus(5);
-    }
-    setBonusToggled(!bonusToggled);
-  };
-
-  const incrementBaleCount = () => {
-    if (baleCount < 5) {
-      setBaleCount(baleCount + 1);
+  const incrementCowCount = () => {
+    if (cowCount < 10) {
+      setCowCount(cowCount + 1);
     }
   };
 
-  const decrementBaleCount = () => {
-    if (baleCount > 0) {
-      setBaleCount(baleCount - 1);
+  const decrementCowCount = () => {
+    if (cowCount > 0) {
+      setCowCount(cowCount - 1);
     }
   };
 
-  const setParkCountValue = (value) => {
-    setParkCount(value);
+  const togglePlotBonus = () => {
+    setClearPlot(!clearPlotBonus);
+  };
+
+  const toggleDoorBonus = () => {
+    setDoor(!doorClosedBonus);
+  };
+
+  const toggleParkBonus = () => {
+    setPark(!parkBonus);
   };
 
   const closeMenu = () => {
@@ -147,18 +145,18 @@ const App = () => {
         <div className="overlay" onClick={handleOverlayClick}>
           <div className="overlay-content">
             <ScoringMenu
-              cropCount={cropCount}
-              incrementCropCount={incrementCropCount}
-              decrementCropCount={decrementCropCount}
-              toggleBonus={toggleBonus}
-              bonusToggled={bonusToggled}
-              baleCount={baleCount}
-              incrementBaleCount={incrementBaleCount}
-              decrementBaleCount={decrementBaleCount}
-              parkCount={parkCount}
-              setParkCountValue={setParkCountValue}
-              toggledButton={toggledButton}
-              setToggledButton={setToggledButton}
+              incrementChickenCount={incrementChickenCount}
+              decrementChickenCount={decrementChickenCount}
+              incrementCowCount={incrementCowCount}
+              decrementCowCount={decrementCowCount}
+              cowCount={cowCount}
+              chickenCount={chickenCount}
+              togglePlotBonus={togglePlotBonus}
+              toggleDoorBonus={toggleDoorBonus}
+              toggleParkBonus={toggleParkBonus}
+              clearPlotBonus={clearPlotBonus}
+              doorClosedBonus={doorClosedBonus}
+              parkBonus={parkBonus}
             />
           </div>
         </div>
--- internal/src/scoring/scoring.jsx
+++ internal/src/scoring/scoring.jsx
@@ -2,53 +2,32 @@ import { useState, useEffect } from 'react';
 import './scoring.css';
 
 const ScoringMenu = ({
-  cropCount, incrementCropCount, decrementCropCount, toggleBonus, bonusToggled,
-  baleCount, incrementBaleCount, decrementBaleCount, parkCount, setParkCountValue,
-  toggledButton, setToggledButton
+  chickenCount, incrementChickenCount, decrementChickenCount,
+  cowCount, incrementCowCount, decrementCowCount,
+  togglePlotBonus, toggleParkBonus, toggleDoorBonus,
+  clearPlotBonus, doorClosedBonus, parkBonus
 }) => {
-
-  const handleParkButtonClick = (value, button) => {
-    setParkCountValue(value);
-    setToggledButton(button);
-  };
 
   return (
     <div className="square-container">
+      <div className="square lightgray">
+        <button onClick={incrementChickenCount}>+</button>
+        <span>Scored Chickens (10 pts)</span>
+        <span>{chickenCount}</span>
+        <button onClick={decrementChickenCount}>-</button>
+      </div>
       <div className="square green">
-        <button onClick={incrementCropCount}>+</button>
-        <span>Crop (1pt)</span>
-        <span>{cropCount}</span>
-        <button onClick={decrementCropCount}>-</button>
-		<div className="button-group">
-          <button className={`${bonusToggled ? 'toggled' : ''}`} onClick={toggleBonus}>Bonus +5pt</button>
-		</div>
+        <button onClick={incrementCowCount}>+</button>
+        <span>Scored Green Cows? (10 pts)</span>
+        <span>{cowCount}</span>
+        <button onClick={decrementCowCount}>-</button>
       </div>
-      <div className="square purple">
-        <button onClick={incrementBaleCount}>+</button>
-        <span>Hay Bale (4pt)</span>
-        <span>{baleCount}</span>
-        <button onClick={decrementBaleCount}>-</button>
-      </div>
-      <div className="square yellow">
-        <span>Park</span>
+      <div className="square">
+        <span>Bonuses</span>
         <div className="button-group">
-		  <button
-            className={toggledButton === 'partial' ? 'toggled' : ''}
-            onClick={() => handleParkButtonClick(2, 'partial')}
-          >
-            Partial +2pt
-          </button>
-          <button
-            className={toggledButton === 'full' ? 'toggled' : ''}
-            onClick={() => handleParkButtonClick(5, 'full')}
-          >
-            Full +5pt
-          </button>
-          <button
-            onClick={() => handleParkButtonClick(0, '')}
-          >
-            Reset
-          </button>
+          <button className={`${clearPlotBonus ? 'blue' : ''}`} onClick={togglePlotBonus}>Plot fully cleared 10 pts</button>
+          <button className={`${doorClosedBonus ? 'red' : ''}`} onClick={toggleDoorBonus}>Door closed 10 pts</button>
+          <button className={`${parkBonus ? 'yellow' : ''}`} onClick={toggleParkBonus}>Fully parked 10 pts</button>
         </div>
       </div>
     </div>
